2f72baa03d4699f68664267ff28cd6338fc4fe57
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-02-26 23:43:46 +0530

    auth

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,6 +11,14 @@ router.get("/email", async (req, res) => {
     return res.status(500).send({ message: err.message });
   }
 });
+router.get("", async (req, res) => {
+  try {
+    const users = await User.find().lean().exec();
+    return res.send(users);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
 
 
 module.exports = router;
