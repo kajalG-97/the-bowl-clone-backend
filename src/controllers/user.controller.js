5d27fa24f173bc305f171ab452fe31062d495bf9
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-02-27 13:00:39 +0530

    mail

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ const User = require('../models/user.model');
 router.get("/email", async (req, res) => {
   try {
     const users = await User.findOne({email: req.query.email}).lean().exec();
-    return res.send({ name:users.first_name });
+    return res.send({ name: users.first_name, mail:users.email});
   } catch (err) {
     return res.status(500).send({ message: err.message });
   }
